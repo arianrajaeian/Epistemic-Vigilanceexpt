081c921204c2a1990deb7121e135d8a5369ecece
Arian Rajaeian <arajaeia@asu.edu>
2025-03-30 12:55:34 -0700

    Make buttons more consistent
    Buttons that said "Advance Study (I'm done reading" now just say "Next"

--- static/scripts/instructions.js
+++ static/scripts/instructions.js
@@ -53,7 +53,7 @@ function nextButton(){
     } else if ($('#Two').is(':visible')){
         $("#Two").hide();
         $("#Three").show();
-        button.textContent = "Advance study (I'm done reading)"
+        button.textContent = "Next"
     } else if ($('#Three').is(':visible')){
         dallinger.goToPage('instructions/mocktrials')
     }
